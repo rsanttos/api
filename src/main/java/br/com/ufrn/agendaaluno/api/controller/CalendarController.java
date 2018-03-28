d56130c49d6266778458a2e2d46b64602c27565d
Ramon Santos <rsanttos22@gmail.com>
2018-03-28 10:03:27 -0300

    Resolve conflitos

--- src/main/java/br/com/ufrn/agendaaluno/api/controller/CalendarController.java
+++ src/main/java/br/com/ufrn/agendaaluno/api/controller/CalendarController.java
@@ -1,8 +1,5 @@
 package br.com.ufrn.agendaaluno.api.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
