52b64d1b7c7975441ae34264f12785ae88ecabdf
Ramon Santos <rsanttos22@gmail.com>
2018-04-24 20:20:14 -0300

    Ajustes no endpoint de calendario

--- src/main/java/br/com/ufrn/agendaaluno/api/controller/CalendarController.java
+++ src/main/java/br/com/ufrn/agendaaluno/api/controller/CalendarController.java
@@ -5,17 +5,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ufrn.agendaaluno.api.dto.AuthenticationDTO;
+import br.com.ufrn.agendaaluno.api.dto.CalendarDTO;
+import br.com.ufrn.agendaaluno.api.dto.HolidayDTO;
 import br.com.ufrn.agendaaluno.api.model.Calendar;
+import br.com.ufrn.agendaaluno.api.request.CalendarRequest;
+import br.com.ufrn.agendaaluno.api.request.HolidayRequest;
+import br.com.ufrn.agendaaluno.api.service.AuthenticationService;
 import br.com.ufrn.agendaaluno.api.service.CalendarService;
 
 @RestController
-public class CalendarController {	
-	@Autowired
-	private CalendarService calendarService;
+public class CalendarController {
 	
+	@Autowired
+	private AuthenticationService authService;
+
 	@RequestMapping(value = "/calendar", method = RequestMethod.GET)
 	public Calendar getCalendar() {
-		Calendar calendar = calendarService.getCalendarByAno(2018);
+		AuthenticationDTO authDto = authService.authenticate();
+
+		CalendarRequest calendarRequest = new CalendarRequest();
+		HolidayRequest holidayRequest = new HolidayRequest();
+
+		CalendarDTO calendarDto = calendarRequest.getCalendar(authDto.getAccess_token());
+		HolidayDTO[] holidaysDto = holidayRequest.getHolidays(authDto.getAccess_token());
+		calendarDto.setHolidays(holidaysDto);
+
+		Calendar calendar = calendarDto.toEntity();
 		return calendar;
 	}
 }
